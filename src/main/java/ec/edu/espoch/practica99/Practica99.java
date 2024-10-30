1b886b89cb4265aa22af89fb9037c19a073e941c
CristhiAn-555 <cristhianvalencia999@gmail.com>
2024-10-30 12:01:23 -0500

    hola

---
+++ src/main/java/ec/edu/espoch/practica99/Practica99.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.practica99;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Practica99 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+       double num1=5;
+         double num2=10;
+         int menu = 1;
+        switch (menu) {
+         case 1:
+             System.out.println("Estas diviendo los dos nuemros");
+            System.out.println("Resultado:" + (num1 / num2));
+          break;
+       
+        }
+        
+    }
+}
